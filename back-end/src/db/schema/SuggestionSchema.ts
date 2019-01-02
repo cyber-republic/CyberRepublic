9e42cb06a80af7ddb97f52d6b45b241b1f74f809
Harry Liu <2411mail@gmail.com>
2019-01-02 17:49:07 +0800

    schema

--- back-end/src/db/schema/SuggestionSchema.ts
+++ back-end/src/db/schema/SuggestionSchema.ts
@@ -1,21 +1,52 @@
 import { Schema } from 'mongoose'
 import { CommentSchema } from './CommentSchema';
 import { SubscriberSchema } from './SubscriberSchema';
+import { constant } from '../../constant';
 
 export const Suggestion = {
-    title: String,
-    desc: String,
-    likes: Number,
-    dislikes: Number,
-    views: Number,
-    activeness: Number,
+    title: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    likes: {
+        type: [Schema.Types.ObjectId],
+        default: [],
+    },
+    likesNum: {
+        type: Number,
+        default: 0,
+    },
+    dislikes: {
+        type: [Schema.Types.ObjectId],
+        default: [],
+    },
+    dislikesNum: {
+        type: Number,
+        default: 0,
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    activeness: {
+        type: Number,
+        default: 0
+    },
     comments: [[CommentSchema]],
     createdBy: {
         type: Schema.Types.ObjectId,
         ref: 'users',
         required: true
     },
-    isAbuse: Boolean,
-    isDelete: Boolean,
+    // constans.SUGGESTION_STATUS: ACTIVE, ABUSED, ARCHIVED. abuse will also be archived
+    status: {
+        type: String,
+        default: constant.SUGGESTION_STATUS.ACTIVE
+    },
+    abusedStatus: String, // constant.SUGGESTION_ABUSED_STATUS: REPORTED, HANDLED
     subscribers: [SubscriberSchema]
 }
