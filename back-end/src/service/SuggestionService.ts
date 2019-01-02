ffa0257ce2f1f3f579bad49b64f7e4aa60c85495
Harry Liu <2411mail@gmail.com>
2019-01-02 17:49:37 +0800

    service

--- back-end/src/service/SuggestionService.ts
+++ back-end/src/service/SuggestionService.ts
@@ -1,19 +1,181 @@
-import Base from './Base';
+import Base from './Base'
+import * as _ from 'lodash'
+import { constant } from '../constant'
+import { validate } from '../utility'
 
 export default class extends Base {
-  public async list(param): Promise<Document> {
-    const db = this.getDBModel('Suggestion')
-    const result = await db.getDBInstance().find()
+  private model: any
+  protected init() {
+    this.model = this.getDBModel('Suggestion')
+  }
+
+  public async create(param: any): Promise<Document> {
+    // get param
+    const { title, desc } = param
+    // validation
+    this.validateTitle(title)
+    this.validateDesc(desc)
+    // build document object
+    const doc = {
+      title,
+      desc,
+      createdBy: _.get(this.currentUser, '_id'),
+    }
+    // save the document
+    return await this.model.save(doc)
+  }
+
+  public async list(param: any): Promise<Document> {
+    // TODO: sort by likes, created, activeness
+    // db.list
+    console.log(param)
+    const cursor = this.model.getDBInstance().find(_.omit(param, ['results', 'page', 'sortBy', 'sortOrder']))
+
+    if (param.sortBy) {
+      const sortObject = {}
+      sortObject[param.sortBy] = param.sortOrder || constant.SORT_ORDER.DESC
+      cursor.sort(sortObject)
+    }
+
+    if (param.results) {
+      const results = parseInt(param.results, 10)
+      const page = parseInt(param.page, 10)
+      cursor.skip(results * (page - 1)).limit(results)
+    }
+
+    const result = await cursor
+      // .populate('createdBy', constant.DB_EXCLUDED_FIELDS.USER)
     return result
   }
 
-  public async create(param): Promise<Document> {
-    const db = this.getDBModel('Suggestion');
-    // get param
-    // validation
-    // build document object
-    // save the document
-    const result = await db.save(param);
-    return result;
+  public async show(param: any): Promise<Document> {
+    return this.model.findById(param.suggestionId).populate('createdBy', constant.DB_EXCLUDED_FIELDS.USER)
+  }
+
+  // like or unlike
+  public async like(param: any): Promise<Document> {
+    console.log('service like: ', param)
+    const { suggestionId: _id } = param
+    const userId = _.get(this.currentUser, '_id')
+    const doc = await this.model.findById(_id)
+    const { likes, dislikes } = doc
+
+    // can not both like and dislike, use ObjectId.equals to compare
+    if (_.findIndex(dislikes, oid => userId.equals(oid)) !== -1) return
+
+    // already liked, will unlike, use ObjectId.equals to compare
+    if (_.findIndex(likes, oid => userId.equals(oid)) !== -1) {
+      await this.model.findOneAndUpdate({ _id }, {
+        $pull: { likes: userId },
+        $inc: { likesNum: -1}
+      })
+    } else {
+      // not like yet, will like it
+      await this.model.findOneAndUpdate({ _id }, {
+        $push: { likes: userId },
+        $inc: { likesNum: 1 }
+      })
+    }
+
+    return this.model.findById(_id)
+  }
+  // dislike <=> undislike
+  // can not both like and dislike
+  public async dislike(param: any): Promise<Document> {
+    const { suggestionId: _id } = param
+    const userId = _.get(this.currentUser, '_id')
+    const doc = await this.model.findById(_id)
+    const { likes, dislikes } = doc
+
+    // can not both like and dislike, use ObjectId.equals to compare
+    if (_.findIndex(likes, oid => userId.equals(oid)) !== -1) return
+
+    // already liked, will unlike, use ObjectId.equals to compare
+    if (_.findIndex(dislikes, oid => userId.equals(oid)) !== -1) {
+      this.model.findOneAndUpdate({ _id }, {
+        $pull: { dislikes: userId },
+        $inc: { dislikesNum: -1 }
+      })
+    } else {
+      // not like yet, will like it
+      this.model.findOneAndUpdate({ _id }, {
+        $push: { dislikes: userId },
+        $inc: { dislikesNum: 1 }
+      })
+    }
+
+    return this.model.findById(_id)
+  }
+
+  // subscribe <=> unfollow
+  public async subscribe(param: any): Promise<Document> {
+    const { suggestionId: _id, willSubscribe } = param
+    const userId = _.get(this.currentUser, '_id')
+    const doc = await this.model.findById(_id)
+    const { subscribers } = doc
+
+    // already followed
+    if (willSubscribe && _.includes(subscribers, userId)) return doc
+
+    // not subscribe yet, will subscribe
+    this.model.findOneAndUpdate({ _id }, {
+      $push: {
+        subscribers: userId
+      }
+    })
+
+    return this.model.findById(_id)
+
+  }
+
+  public async reportabuse(param: any): Promise<Document> {
+    const { suggestionId: _id } = param
+    const updateObject = {
+      abusedStatus: constant.SUGGESTION_ABUSED_STATUS.REPORTED
+    }
+    this.model.findOneAndUpdate({ _id }, updateObject)
+    return this.model.findById(_id)
+  }
+
+  /**
+   * Admin only
+   */
+  public async abuse(param: any): Promise<Document> {
+    // TODO: checkPermission admin
+    const { suggestionId: _id } = param
+    const updateObject = {
+      status: constant.SUGGESTION_STATUS.ABUSED,
+      abusedStatus: constant.SUGGESTION_ABUSED_STATUS.HANDLED
+    }
+    this.model.findOneAndUpdate({ _id }, updateObject)
+    return this.model.findById(_id)
+  }
+
+  public async archive(param: any): Promise<Document> {
+    // TODO: checkPermission admin
+    const { suggestionId: _id } = param
+    const updateObject = {
+      status: constant.SUGGESTION_STATUS.ARCHIVED,
+    }
+    this.model.findOneAndUpdate({ _id }, updateObject)
+    return this.model.findById(_id)
+  }
+
+  public async delete(param: any): Promise<Document> {
+    // TODO: checkPermission admin
+    const { suggestionId: _id } = param
+    return this.model.findByIdAndDelete(_id)
+  }
+
+  public validateTitle(title) {
+    if (!validate.valid_string(title, 4)) {
+      throw 'invalid title'
+    }
+  }
+
+  public validateDesc(desc) {
+    if (!validate.valid_string(desc, 1)) {
+      throw 'invalid description'
+    }
   }
 }
