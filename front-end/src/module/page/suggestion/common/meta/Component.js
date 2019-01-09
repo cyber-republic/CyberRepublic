a6de775ede808600033d77a5557b63f6a35e0bf9
Harry Liu <2411mail@gmail.com>
2019-01-09 19:32:14 +0800

    can hide author

--- front-end/src/module/page/suggestion/common/meta/Component.js
+++ front-end/src/module/page/suggestion/common/meta/Component.js
@@ -5,14 +5,15 @@ import I18N from '@/I18N'
 
 import './style.scss'
 
-export default ({ data }) => {
+export default ({ data, hideAuthor }) => {
     const { displayId, createdAt } = data;
     const author = data.author || `${_.get(data, 'createdBy.profile.firstName')} ${_.get(data, 'createdBy.profile.lastName')}`
+    const authorNode = hideAuthor ? '' : <span>{I18N.get('suggestion.postedBy')} {author}</span>
 
     return (
         <div className='c_SuggestionMeta'>
             <span>{`#${displayId}`}</span>
-            <span>{I18N.get('suggestion.postedBy')} {author}</span>
+            {authorNode}
             <span>{moment(createdAt).format('MMM D, YYYY')}</span>
         </div>
     )
